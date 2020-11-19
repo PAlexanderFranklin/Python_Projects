0bee3671aad460bf9db1823d43333209264d813d
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2020-11-19 11:34:46 -0800

    Wrap click loop in function, Change clickList structure

--- Clicking.py
+++ Clicking.py
@@ -2,16 +2,20 @@ import pyautogui
 import time
 
 clickList = [] # (x position of click, y position of click, time delay in seconds)
-clickList.append({'x': 1480, 'y': 250, 't': 0.2})
-clickList.append({'x': 1650, 'y': 390, 't': 0.2})
+clickList.append([1480, 250, 0.05])
+clickList.append([1650, 280, 0.05])
 
-try:
-    for i in clickList:
-        pyautogui.moveTo(i['x'], i['y'])
-        position = pyautogui.position()
-        time.sleep(i['t'])
-        if pyautogui.position() != position:
-            raise RuntimeError("Failsafe activated")
-        pyautogui.click()
-except RuntimeError as message:
-    print(message)
+def repeatClicks(clickList, repeats):
+    try:
+        for n in range(0, repeats):
+            for i in clickList:
+                pyautogui.moveTo(i[0], i[1])
+                position = pyautogui.position()
+                time.sleep(i[2])
+                if pyautogui.position() != position:
+                    raise RuntimeError("Mouse moved by user, activating failsafe.")
+                pyautogui.click()
+    except RuntimeError as message:
+        print(message)
+
+repeatClicks(clickList, 50)
