f75aea35131d592abb059def816c72227f4539ba
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2020-11-19 09:34:16 -0800

    Add failsafe

--- Clicking.py
+++ Clicking.py
@@ -1,3 +1,13 @@
 import pyautogui
-import sys
 import time
+
+# click locations: 1480, 250 => 1650, 390
+
+try:
+    for i in range(0, 1000):
+        position = pyautogui.position()
+        time.sleep(0.05)
+        if pyautogui.position() != position:
+            raise RuntimeError("Failsafe activated")
+except RuntimeError as message:
+    print(message)
