a23ede22b96bfeedf4f9ba5a7a6c2ff4dd9c0255
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2021-07-02 14:45:31 -0700

    Add longest non-audiobook track query

--- SQLiteQueries.py
+++ SQLiteQueries.py
@@ -4,15 +4,28 @@ db = apsw.Connection("./SQLiteDB/chinook.db")
 cursor1 = db.cursor()
 cursor1.execute("""
 SELECT *
-FROM employees;
+FROM employees
+LIMIT 10;
 """)
-#print(cursor1.fetchall())
+print(cursor1.fetchall(), '\n')
 
 cursor2 = db.cursor()
 cursor2.execute("""
 SELECT Composer, SUM(Milliseconds)
 FROM tracks
+WHERE MediaTypeId != 3
 GROUP BY Composer
+ORDER BY SUM(Milliseconds) DESC
 LIMIT 10;
 """)
-print(cursor2.fetchall())
+print(cursor2.fetchall(), '\n')
+
+cursor3 = db.cursor()
+cursor3.execute("""
+SELECT Name, MediaTypeId, Milliseconds
+FROM tracks
+WHERE MediaTypeId != 3
+ORDER BY Milliseconds DESC
+LIMIT 10
+""")
+print(cursor3.fetchall(), '\n')
