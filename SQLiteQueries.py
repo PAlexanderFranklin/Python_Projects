b11e05ed22cbeaa1f976d80a010efd69889a1d5f
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2021-07-02 01:03:25 -0700

    Add SQL sum example

--- SQLiteQueries.py
+++ SQLiteQueries.py
@@ -4,6 +4,15 @@ db = apsw.Connection("./SQLiteDB/chinook.db")
 cursor1 = db.cursor()
 cursor1.execute("""
 SELECT *
-FROM employees
+FROM employees;
 """)
-print(cursor1.fetchall())
+#print(cursor1.fetchall())
+
+cursor2 = db.cursor()
+cursor2.execute("""
+SELECT Composer, SUM(Milliseconds)
+FROM tracks
+GROUP BY Composer
+LIMIT 10;
+""")
+print(cursor2.fetchall())
